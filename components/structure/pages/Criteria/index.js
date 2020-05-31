b302fc24d46b78549b19db9b32c4cae15960006d
phelios <phelio.xion@gmail.com>
2020-05-31 11:57:44 +1000

    [WIP] [Refactoring] Rehook handleNew method

--- components/structure/pages/Criteria/index.js
+++ components/structure/pages/Criteria/index.js
@@ -17,95 +17,13 @@ import {
 } from 'native-base';
 import {Modal, View} from 'react-native';
 import {flexStyle, mainStyle} from '../../../layout/style';
-import CrudView from '../../../tbd/flatlist';
+import CrudView from '../../../tbd/CrudView';
 
 export default function Criteria() {
   const [criteria, setCriteria] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
 
-  function handleNew() {
-    const formData = {
-      name: tname,
-      weight: tweight,
-    };
 
-    let data = {
-      method: 'POST',
-      body: JSON.stringify(formData),
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    };
-
-    return fetch('https://dm-api.herokuapp.com/api/criteria/', data)
-      .then(response => response.json()) // promise
-      .then(json => {
-        setCriteria([...criteria, json]);
-        closeModal();
-      })
-      .then(err => {
-        console.log(err);
-      });
-  }
-
-  function handleDelete(itemId) {
-    let data = {
-      method: 'DELETE',
-    };
-    return fetch(`https://dm-api.herokuapp.com/api/criteria/${itemId}`, data)
-      .then(response => {
-        setCriteria(
-          criteria.filter(v => {
-            return v.id !== itemId;
-          }),
-        );
-        closeModal();
-      })
-      .then(err => {
-        console.log(err);
-      });
-  }
-
-  function handleUpdate(itemId) {
-    const formData = {
-      name: tname,
-      weight: tweight,
-    };
-
-    let data = {
-      method: 'PUT',
-      body: JSON.stringify(formData),
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    };
-    return fetch(`https://dm-api.herokuapp.com/api/criteria/${itemId}`, data)
-      .then(response => {
-        console.log(response);
-
-        const idx = criteria.findIndex(i => i.id === itemId);
-        criteria[idx] = {
-          id: tid,
-          name: tname,
-          weight: tweight,
-        };
-        setCriteria(criteria);
-        closeModal();
-      })
-      .then(err => {
-        console.log(err);
-      });
-  }
-
-  async function fetchData() {
-    const res = await fetch('https://dm-api.herokuapp.com/api/criteria/');
-    res
-      .json()
-      .then(res => setCriteria(res))
-      .then(err => console.log(err));
-  }
 
   // function renderRow(data) {
   //   const {item} = data;
@@ -120,10 +38,6 @@ export default function Criteria() {
   //     </ListItem>
   //   );
   // }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   const [name, weight] = ['name', 'weight'];
 
@@ -140,7 +54,11 @@ export default function Criteria() {
           <Title>Criteria</Title>
         </Body>
       </Header>
-      <CrudView data={criteria} displayFields={listDisplays} fields={fields} />
+      <CrudView
+        displayFields={listDisplays}
+        fields={fields}
+        url={'https://dm-api.herokuapp.com/api/criteria/'}
+      />
     </Container>
   );
 }
---
+++ components/tbd/CrudView/index.js
@@ -0,0 +1,215 @@
+import {FlatList, Modal, View} from 'react-native';
+import {
+  Body,
+  Button,
+  Container,
+  Content,
+  Form,
+  Header,
+  Input,
+  Item,
+  Label,
+  Left,
+  ListItem,
+  Right,
+  Text,
+  Title,
+} from 'native-base';
+import React, {useEffect, useState} from 'react';
+import {flexStyle} from '../../layout/style';
+
+export default function CrudView(props) {
+  const {fields, displayFields, url} = props;
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [listData, setListData] = useState([]);
+
+  const initData = fields.reduce((obj, val) => {
+    obj[val] = null;
+    return obj;
+  }, {});
+
+  const [formData, setFormData] = useState(initData);
+
+  function renderEdit(item) {
+    console.log(item);
+    setModalVisible(true);
+    // setName(item.name);
+    // setWeight(item.weight.toString());
+    // setId(item.id);
+  }
+
+  function renderRow(data) {
+    const {item} = data;
+    return (
+      <ListItem onPress={() => renderEdit(item)}>
+        <Left>
+          <Text>{item[displayFields.left]}</Text>
+        </Left>
+        <Right>
+          <Text>{item[displayFields.right]}</Text>
+        </Right>
+      </ListItem>
+    );
+  }
+
+  function resetData() {
+    setFormData(initData);
+  }
+
+  function closeModal() {
+    resetData();
+    setModalVisible(false);
+  }
+
+  function updateFormData(fieldName, text) {
+    setFormData({...formData, [fieldName]: text});
+  }
+
+  // function _fetch(handler, data, _url) {
+  //   fetch(_url, data)
+  //     .then(response => resonse.json() )
+  //     .then(json => console.log(json))
+  //     // .then(r => handler(r))
+  //     // .then(closeModal());
+  // }
+
+  function handleNew() {
+    let data = {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    };
+
+    fetch(url, data)
+      .then(r => r.json())
+      .then(json => setListData([...listData, json]))
+      .then(closeModal);
+  }
+
+  function handleDelete(itemId) {
+    let data = {
+      method: 'DELETE',
+    };
+    return fetch(`${url}/${itemId}`, data)
+      .then(response => {
+        setFormData(
+          formData.filter(v => {
+            return v.id !== itemId;
+          }),
+        );
+        closeModal();
+      })
+      .then(err => {
+        console.log(err);
+      });
+  }
+
+  function handleUpdate(itemId) {
+    const formData = {
+      name: formData.name,
+      weight: formData.weight,
+    };
+
+    let data = {
+      method: 'PUT',
+      body: JSON.stringify(formData),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    };
+    return fetch(`https://dm-api.herokuapp.com/api/criteria/${itemId}`, data)
+      .then(response => {
+        console.log(response);
+
+        const idx = listData.findIndex(i => i.id === itemId);
+        listData[idx] = {
+          id: formData.id,
+          name: formData.name,
+          weight: formData.weight,
+        };
+        setListData(listData);
+        closeModal();
+      })
+      .then(err => {
+        console.log(err);
+      });
+  }
+
+  /* eslint-disable react-hooks/exhaustive-deps */
+  useEffect(() => {
+    const res = fetch(url);
+    res
+      .then(data => data.json())
+      .then(json => setListData(json))
+      .then(err => console.log(err));
+  }, []);
+  /* eslint-enable react-hooks/exhaustive-deps */
+
+  function renderModalButtons() {
+    if (formData.id) {
+      return (
+        <>
+          <Button style={flexStyle.button} onPress={handleUpdate}>
+            <Text>Update</Text>
+          </Button>
+          <Button danger style={flexStyle.button} onPress={handleDelete}>
+            <Text>Delete</Text>
+          </Button>
+        </>
+      );
+    } else {
+      return (
+        <Button style={flexStyle.button} onPress={handleNew}>
+          <Text>Add</Text>
+        </Button>
+      );
+    }
+  }
+
+  return (
+    <>
+      <FlatList
+        data={listData}
+        renderItem={renderRow}
+        keyExtractor={item => item.id.toString()}
+      />
+      <Button block onPress={() => setModalVisible(true)}>
+        <Text>Add</Text>
+      </Button>
+      <Modal animationType="slide" transparent={true} visible={modalVisible}>
+        <Container>
+          <Header>
+            <Left />
+            <Body>
+              <Title>Add Criteria</Title>
+            </Body>
+            <Right>
+              <Button danger transparent onPress={() => closeModal()}>
+                <Text>close</Text>
+              </Button>
+            </Right>
+          </Header>
+          <Content padder>
+            <Form>
+              {fields.map(field => (
+                <Item stackedLabel key={field}>
+                  <Label style={flexStyle.label}>{field}</Label>
+                  <Input
+                    value={formData[field]}
+                    onChangeText={t => updateFormData(field, t)}
+                  />
+                </Item>
+              ))}
+            </Form>
+            <View style={flexStyle.rowFlex}>{renderModalButtons()}</View>
+          </Content>
+        </Container>
+      </Modal>
+    </>
+  );
+}
